5c55992f919bd9e9496ccdeac4f6b262c41151c0
djoseph1250 <djoseph1250@gmail.com>
2017-06-21 21:25:42 -0400

    added calendar filter

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -4,7 +4,11 @@ import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
 import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
 import { Meteor } from 'meteor/meteor';
+
+import ChatsCtrl from '../controllers/chats.controller';
+import RoutesConfig from '../routes';
 
 const App = 'Whuzzapp';
 
@@ -12,6 +16,8 @@ Angular.module(App, [
     'angular-meteor',
     'ionic'
 ]);
+
+new Loader(app).load(RoutesConfig);
 
 if (Meteor.isCordova) {
     Angular.element(docuq).on('deviceready', onReady);
